218a1bc0624ea7c4ead1ddc5b55169ded59d6484
jinhui <365320229@qq.com>
2020-08-23 11:06:50 +0800

    fix: 改造为NACOS存储规则 https://github.com/eacdy/Sentinel-Dashboard-Nacos/tree/1.6.2-NACOS/sentinel-dashboard

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -20,22 +20,35 @@ import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.config.ConfigFactory;
 import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author www.itmuch.com
  */
 @Configuration
 public class NacosConfig {
+
+    @Value("${NACOS_ADDR}")
+    private String nacosAddr;
+
+    @Value("${NACOS_NAMESPACE:public}")
+    private String nacosNameSpace;
+
     @Bean
     public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
         return JSON::toJSONString;
     }
+
     @Bean
     public ConfigService nacosConfigService() throws Exception {
-        return ConfigFactory.createConfigService("localhost");
+        Properties properties = new Properties();
+        properties.put("serverAddr", nacosAddr);
+        properties.put("namespace", nacosNameSpace);
+        return ConfigFactory.createConfigService(properties);
     }
 }
